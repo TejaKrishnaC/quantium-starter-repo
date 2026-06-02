b98549e50cc1b6db374176f345462fce99fdf016
CTejaKrishna <tejashield2003@gmail.com>
2026-06-02 23:13:33 +0530

    completed task and generated output file

---
+++ dataproce.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+df0=pd.read_csv("data/daily_sales_data_0.csv")
+df1=pd.read_csv("data/daily_sales_data_0.csv")
+df2=pd.read_csv("data/daily_sales_data_0.csv")
+combined=pd.concat([df0,df1,df2],ignore_index=True)
+pink_morsel=combined[combined["product"].str.lower()=="pink_morsel"].copy()
+pink_morsel["price"]=pink_morsel["price"].astype(str).str.replace("$","",regex=False).astype(float)
+pink_morsel["quantity"]=pink_morsel["quantity"].astype(int)
+pink_morsel["sales"]=pink_morsel["price"]*pink_morsel["quantity"]
+final=pink_morsel[["sales","date","region"]]
+final.to_csv("formatted",index=False)
+print("Success")
